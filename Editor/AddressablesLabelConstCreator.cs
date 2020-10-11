cdbaf5799857a75221cfb4a2b5539dafbf3a03b8
baba_s <shotababa1206@gmail.com>
2020-10-11 19:00:14 +0900

    first commit

---
+++ Editor/AddressablesLabelConstCreator.cs
@@ -0,0 +1,85 @@
+﻿using Kogane.Internal;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEngine;
+
+namespace Kogane
+{
+	/// <summary>
+	/// Addressables のラベルを文字列の定数で管理するクラスを生成するエディタ拡張
+	/// </summary>
+	public static class AddressablesLabelConstCreator
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// コードを生成します
+		/// </summary>
+		[MenuItem( "Edit/" + AddressablesLabelConstCreatorSettings.PACKAGE_NAME + "/コード生成" )]
+		public static void Create()
+		{
+			var settings = AddressablesLabelConstCreatorSettings.GetInstance();
+
+			Create
+			(
+				codeTemplate: settings.CodeTemplate,
+				outputAssetPath: settings.OutputAssetPath
+			);
+		}
+
+		/// <summary>
+		/// コードを生成します
+		/// </summary>
+		public static void Create( string codeTemplate, string outputAssetPath )
+		{
+			var labels = AddressableAssetSettingsDefaultObject.Settings.GetLabels();
+
+			var values = labels
+					.Where( x => !string.IsNullOrWhiteSpace( x ) )
+					.Select
+					(
+						x => new ConstStringCodeGeneratorOptions.Element
+						{
+							Name    = x.ToUpper(),
+							Comment = x,
+							Value   = x,
+						}
+					)
+					.ToArray()
+				;
+
+			var options = new ConstStringCodeGeneratorOptions
+			{
+				Template = codeTemplate,
+				Elements = values,
+			};
+
+			var code = ConstStringCodeGenerator
+					.Generate( options )
+					.Replace( "\t", "    " )
+					.Replace( "\r\n", "#NEW_LINE#" )
+					.Replace( "\r", "#NEW_LINE#" )
+					.Replace( "\n", "#NEW_LINE#" )
+					.Replace( "#NEW_LINE#", "\r\n" )
+				;
+
+			ConstStringCodeGenerator.Write( outputAssetPath, code );
+			AssetDatabase.Refresh();
+
+			var filename = Path.GetFileName( outputAssetPath );
+			var asset    = AssetDatabase.LoadAssetAtPath<TextAsset>( outputAssetPath );
+
+			EditorUtility.DisplayDialog
+			(
+				AddressablesLabelConstCreatorSettings.PACKAGE_NAME,
+				$"{filename} の作成が完了しました。",
+				"OK"
+			);
+
+			EditorGUIUtility.PingObject( asset );
+		}
+	}
+}
---
+++ Editor/AddressablesLabelConstCreatorSettings.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	/// <summary>
+	/// Preferences における設定を管理する ScriptableObject
+	/// </summary>
+	internal sealed class AddressablesLabelConstCreatorSettings
+		: ScriptableObjectForProjectSettings<AddressablesLabelConstCreatorSettings>
+	{
+		//================================================================================
+		// 定数
+		//================================================================================
+		public const string PACKAGE_NAME = "UniAddressablesLabelConstCreator";
+
+		private const string DEFAULT_OUTPUT_ASSET_PATH = "Assets/AddressablesLabelConst.cs";
+
+		private const string DEFAULT_CODE_TEMPLATE = @"using System.Collections.Generic;
+
+namespace Kogane
+{
+    public static partial class AddressablesLabelConst
+    {
+        public const int LENGTH = #LENGTH#;
+
+#VALUES#
+
+        public static IEnumerable<string> GetValues()
+        {
+#GET_VALUES_CONTENTS#
+        }
+    }
+}";
+
+		//================================================================================
+		// 変数(static)
+		//================================================================================
+		[SerializeField]                  private string m_outputAssetPath = DEFAULT_OUTPUT_ASSET_PATH;
+		[SerializeField][Multiline( 16 )] private string m_codeTemplate    = DEFAULT_CODE_TEMPLATE;
+
+		//================================================================================
+		// プロパティ
+		//================================================================================
+		public string OutputAssetPath => m_outputAssetPath;
+		public string CodeTemplate    => m_codeTemplate;
+
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		[SettingsProvider]
+		private static SettingsProvider SettingsProvider()
+		{
+			return CreateSettingsProvider
+			(
+				settingsProviderPath: $"Kogane/{PACKAGE_NAME}",
+				onGUIExtra: so =>
+				{
+					if ( GUILayout.Button( "Reset to Default" ) )
+					{
+						so.FindProperty( nameof( m_outputAssetPath ) ).stringValue = DEFAULT_OUTPUT_ASSET_PATH;
+						so.FindProperty( nameof( m_codeTemplate ) ).stringValue    = DEFAULT_CODE_TEMPLATE;
+					}
+
+					if ( GUILayout.Button( "Create Code" ) )
+					{
+						AddressablesLabelConstCreator.Create();
+					}
+				}
+			);
+		}
+	}
+}
